c9f1999349685a64be09424c3b085b424543afb0
Aidan-Stanly <rstanlyjoseph@gmail.com>
2021-06-24 20:18:10 +1000

    modified

--- BaseClass.js
+++ BaseClass.js
@@ -9,15 +9,4 @@ class BaseClass{
         this.width = width;
         this.height = height;
         this.image = loadImage("sprites/base.png");
-        World.add(world, this.body);
-      }
-      display(){
-        var angle = this.body.angle;
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
-        imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);
-        pop();
-      }
-}
+        Wo
--- Pig.js
+++ Pig.js
@@ -1,7 +1,2 @@
 class Pig extends BaseClass {
-  constructor(x, y){
-    super(x,y,50,50);
-    this.image = loadImage("sprites/enemy.png");
-  }
-
-};
+  constructor(x, 
